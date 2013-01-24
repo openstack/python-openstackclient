0a4912f9fb07a7330a2527af4cc7c8949c685401
Josh Kearney <josh@jk0.org>
2013-01-24 12:00:30 -0600

    Standardize on a copyright header and ensure all files have them.
    Change-Id: I64812bca01ca655c9cf9239a0daea84907082a29

--- openstackclient/image/client.py
+++ openstackclient/image/client.py
@@ -1,4 +1,4 @@
-#   Copyright 2012 OpenStack, LLC.
+#   Copyright 2012-2013 OpenStack, LLC.
 #
 #   Licensed under the Apache License, Version 2.0 (the "License"); you may
 #   not use this file except in compliance with the License. You may obtain
