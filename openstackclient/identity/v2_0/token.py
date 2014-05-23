d6321c0893d529af1548da79a985f337bce7069f
Terry Howe <terrylhowe@gmail.com>
2014-05-23 10:17:42 -0600

    Add token delete command for identity v2
    Identity v2 has undocumented support for token delete and
    keystoneclient also has support.
    
    Change-Id: Ib98d17958ceb88f7b63471691dee71fdb884ce2e
    Closes-Bug: #1318442

--- openstackclient/identity/v2_0/token.py
+++ openstackclient/identity/v2_0/token.py
@@ -18,6 +18,7 @@
 import logging
 import six
 
+from cliff import command
 from cliff import show
 
 
@@ -36,3 +37,24 @@ class CreateToken(show.ShowOne):
         token = identity_client.service_catalog.get_token()
         token['project_id'] = token.pop('tenant_id')
         return zip(*sorted(six.iteritems(token)))
+
+
+class DeleteToken(command.Command):
+    """Delete token command"""
+
+    log = logging.getLogger(__name__ + '.DeleteToken')
+
+    def get_parser(self, prog_name):
+        parser = super(DeleteToken, self).get_parser(prog_name)
+        parser.add_argument(
+            'token',
+            metavar='<token>',
+            help='Token to be deleted',
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        self.log.debug('take_action(%s)' % parsed_args)
+        identity_client = self.app.client_manager.identity
+        identity_client.tokens.delete(parsed_args.token)
+        return
