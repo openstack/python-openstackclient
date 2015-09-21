e0e9b2bfaee72a8b7c0619faefc86f1bbcdf7414
Steve Martinelli <stevemar@ca.ibm.com>
2015-09-21 15:13:48 -0400

    Use format_list instead of format_dict when listing images
    This currently breaks listing images with --long. Tags are an
    array and shouldn't be formatted as a dictionary.
    
    Change-Id: I6d1d85351b58ae4824498774673ebdc8eaa7e420
    Closes-Bug: #1498150

--- openstackclient/image/v2/image.py
+++ openstackclient/image/v2/image.py
@@ -229,7 +229,7 @@ class ListImage(lister.Lister):
                 s,
                 columns,
                 formatters={
-                    'tags': utils.format_dict,
+                    'tags': utils.format_list,
                 },
             ) for s in data)
         )
