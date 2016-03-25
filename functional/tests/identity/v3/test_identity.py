add0e1002624630ce858a1c2b2199a3c22f39da6
Kristi Nikolla <knikolla@bu.edu>
2016-03-25 12:12:55 -0400

    Added functional tests for 'service provider' v3 commands
    'identity provider' commands had functional tests but 'service
    provider' commands did not. Added the tests in a similar way to
    how it is done it test_idp.
    
    Change-Id: Id4b24ef7d34db65c6b0260c89327ec9be683284d

--- functional/tests/identity/v3/test_identity.py
+++ functional/tests/identity/v3/test_identity.py
@@ -44,6 +44,11 @@ class IdentityTests(test.TestCase):
     IDENTITY_PROVIDER_FIELDS = ['description', 'enabled', 'id', 'remote_ids']
     IDENTITY_PROVIDER_LIST_HEADERS = ['ID', 'Enabled', 'Description']
 
+    SERVICE_PROVIDER_FIELDS = ['auth_url', 'description', 'enabled',
+                               'id', 'relay_state_prefix', 'sp_url']
+    SERVICE_PROVIDER_LIST_HEADERS = ['ID', 'Enabled', 'Description',
+                                     'Auth URL']
+
     @classmethod
     def setUpClass(cls):
         if hasattr(super(IdentityTests, cls), 'setUpClass'):
@@ -269,3 +274,22 @@ class IdentityTests(test.TestCase):
                 self.openstack,
                 'identity provider delete %s' % identity_provider)
         return identity_provider
+
+    def _create_dummy_sp(self, add_clean_up=True):
+        service_provider = data_utils.rand_name('ServiceProvider')
+        description = data_utils.rand_name('description')
+        raw_output = self.openstack(
+            'service provider create '
+            ' %(name)s '
+            '--description %(description)s '
+            '--auth-url https://sp.example.com:35357 '
+            '--service-provider-url https://sp.example.com:5000 '
+            '--enable ' % {'name': service_provider,
+                           'description': description})
+        items = self.parse_show(raw_output)
+        self.assert_show_fields(items, self.SERVICE_PROVIDER_FIELDS)
+        if add_clean_up:
+            self.addCleanup(
+                self.openstack,
+                'service provider delete %s' % service_provider)
+        return service_provider
