393ecedc3b162ad166d2be92485ec985e8f7ae0e
Thomas Goirand <zigo@debian.org>
2016-07-11 14:53:37 +0200

    Make the doc build reproducible
    This fixes Debian bug:
    https://bugs.debian.org/826676
    
    Change-Id: I638488448a4d6e644ab0e6deaebd65c92fa7904a

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -80,7 +80,7 @@ release = version_info.release_string()
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = ['**tests**']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
