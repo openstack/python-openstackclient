e2a9fd29c118351cd2c99e40233c9eb31c760154
Steve Martinelli <s.martinelli@gmail.com>
2016-07-16 13:33:03 -0400

    Follow upper constraints for all tox targets
    With the exception of releasenotes and cover, we should follow
    upper constraints. The tox_install file was copied over from
    python-neutronclient [1].
    
    [1] http://git.openstack.org/cgit/openstack/python-neutronclient/tree/tools/tox_install.sh
    
    Change-Id: I633fa149820efafd7b2acec0388fa8bc8d06c988

---
+++ tools/tox_install.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+# Client constraint file contains this client version pin that is in conflict
+# with installing the client from source. We should replace the version pin in
+# the constraints file before applying it for from-source installation.
+
+ZUUL_CLONER=/usr/zuul-env/bin/zuul-cloner
+BRANCH_NAME=master
+CLIENT_NAME=python-openstackclient
+requirements_installed=$(echo "import openstack_requirements" | python 2>/dev/null ; echo $?)
+
+set -e
+
+CONSTRAINTS_FILE=$1
+shift
+
+install_cmd="pip install"
+if [ $CONSTRAINTS_FILE != "unconstrained" ]; then
+
+    mydir=$(mktemp -dt "$CLIENT_NAME-tox_install-XXXXXXX")
+    localfile=$mydir/upper-constraints.txt
+    if [[ $CONSTRAINTS_FILE != http* ]]; then
+        CONSTRAINTS_FILE=file://$CONSTRAINTS_FILE
+    fi
+    curl $CONSTRAINTS_FILE -k -o $localfile
+    install_cmd="$install_cmd -c$localfile"
+
+    if [ $requirements_installed -eq 0 ]; then
+        echo "ALREADY INSTALLED" > /tmp/tox_install.txt
+        echo "Requirements already installed; using existing package"
+    elif [ -x "$ZUUL_CLONER" ]; then
+        export ZUUL_BRANCH=${ZUUL_BRANCH-$BRANCH}
+        echo "ZUUL CLONER" > /tmp/tox_install.txt
+        pushd $mydir
+        $ZUUL_CLONER --cache-dir \
+            /opt/git \
+            --branch $BRANCH_NAME \
+            git://git.openstack.org \
+            openstack/requirements
+        cd openstack/requirements
+        $install_cmd -e .
+        popd
+    else
+        echo "PIP HARDCODE" > /tmp/tox_install.txt
+        if [ -z "$REQUIREMENTS_PIP_LOCATION" ]; then
+            REQUIREMENTS_PIP_LOCATION="git+https://git.openstack.org/openstack/requirements@$BRANCH_NAME#egg=requirements"
+        fi
+        $install_cmd -U -e ${REQUIREMENTS_PIP_LOCATION}
+    fi
+
+    edit-constraints $localfile -- $CLIENT_NAME "-e file://$PWD#egg=$CLIENT_NAME"
+fi
+
+$install_cmd -U $*
+exit $?
