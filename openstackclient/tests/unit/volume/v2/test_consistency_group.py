98b9bc10d1ab6c205dee7ca3813034ccd1de1005
Huanxuan Ao <huanxuan.ao@easystack.cn>
2016-11-10 21:26:19 +0800

    Add "consistency group create" command
    Add "consistency group create" command in
    volume v2 (v2 only).
    
    Change-Id: I2e9affe390b1012aa18459e64d04afcdfc15e27d
    Implements: bp cinder-command-support
    Partial-Bug: #1613964

--- openstackclient/tests/unit/volume/v2/test_consistency_group.py
+++ openstackclient/tests/unit/volume/v2/test_consistency_group.py
@@ -28,6 +28,138 @@ class TestConsistencyGroup(volume_fakes.TestVolume):
             self.app.client_manager.volume.consistencygroups)
         self.consistencygroups_mock.reset_mock()
 
+        self.types_mock = self.app.client_manager.volume.volume_types
+        self.types_mock.reset_mock()
+
+
+class TestConsistencyGroupCreate(TestConsistencyGroup):
+
+    volume_type = volume_fakes.FakeType.create_one_type()
+    new_consistency_group = (
+        volume_fakes.FakeConsistencyGroup.create_one_consistency_group())
+
+    columns = (
+        'availability_zone',
+        'created_at',
+        'description',
+        'id',
+        'name',
+        'status',
+        'volume_types',
+    )
+    data = (
+        new_consistency_group.availability_zone,
+        new_consistency_group.created_at,
+        new_consistency_group.description,
+        new_consistency_group.id,
+        new_consistency_group.name,
+        new_consistency_group.status,
+        new_consistency_group.volume_types,
+    )
+
+    def setUp(self):
+        super(TestConsistencyGroupCreate, self).setUp()
+        self.consistencygroups_mock.create.return_value = (
+            self.new_consistency_group)
+        self.consistencygroups_mock.create_from_src.return_value = (
+            self.new_consistency_group)
+        self.consistencygroups_mock.get.return_value = (
+            self.new_consistency_group)
+        self.types_mock.get.return_value = self.volume_type
+
+        # Get the command object to test
+        self.cmd = consistency_group.CreateConsistencyGroup(self.app, None)
+
+    def test_consistency_group_create(self):
+        arglist = [
+            '--volume-type', self.volume_type.id,
+            '--description', self.new_consistency_group.description,
+            '--availability-zone',
+            self.new_consistency_group.availability_zone,
+            self.new_consistency_group.name,
+        ]
+        verifylist = [
+            ('volume_type', self.volume_type.id),
+            ('description', self.new_consistency_group.description),
+            ('availability_zone',
+             self.new_consistency_group.availability_zone),
+            ('name', self.new_consistency_group.name),
+        ]
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+
+        columns, data = self.cmd.take_action(parsed_args)
+
+        self.types_mock.get.assert_called_once_with(
+            self.volume_type.id)
+        self.consistencygroups_mock.get.assert_not_called()
+        self.consistencygroups_mock.create.assert_called_once_with(
+            self.volume_type.id,
+            name=self.new_consistency_group.name,
+            description=self.new_consistency_group.description,
+            availability_zone=self.new_consistency_group.availability_zone,
+        )
+
+        self.assertEqual(self.columns, columns)
+        self.assertEqual(self.data, data)
+
+    def test_consistency_group_create_without_name(self):
+        arglist = [
+            '--volume-type', self.volume_type.id,
+            '--description', self.new_consistency_group.description,
+            '--availability-zone',
+            self.new_consistency_group.availability_zone,
+        ]
+        verifylist = [
+            ('volume_type', self.volume_type.id),
+            ('description', self.new_consistency_group.description),
+            ('availability_zone',
+             self.new_consistency_group.availability_zone),
+        ]
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+
+        columns, data = self.cmd.take_action(parsed_args)
+
+        self.types_mock.get.assert_called_once_with(
+            self.volume_type.id)
+        self.consistencygroups_mock.get.assert_not_called()
+        self.consistencygroups_mock.create.assert_called_once_with(
+            self.volume_type.id,
+            name=None,
+            description=self.new_consistency_group.description,
+            availability_zone=self.new_consistency_group.availability_zone,
+        )
+
+        self.assertEqual(self.columns, columns)
+        self.assertEqual(self.data, data)
+
+    def test_consistency_group_create_from_source(self):
+        arglist = [
+            '--consistency-group-source', self.new_consistency_group.id,
+            '--description', self.new_consistency_group.description,
+            self.new_consistency_group.name,
+        ]
+        verifylist = [
+            ('consistency_group_source', self.new_consistency_group.id),
+            ('description', self.new_consistency_group.description),
+            ('name', self.new_consistency_group.name),
+        ]
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+
+        columns, data = self.cmd.take_action(parsed_args)
+
+        self.types_mock.get.assert_not_called()
+        self.consistencygroups_mock.get.assert_called_once_with(
+            self.new_consistency_group.id)
+        self.consistencygroups_mock.create_from_src.assert_called_with(
+            None,
+            self.new_consistency_group.id,
+            name=self.new_consistency_group.name,
+            description=self.new_consistency_group.description,
+        )
+
+        self.assertEqual(self.columns, columns)
+        self.assertEqual(self.data, data)
+
 
 class TestConsistencyGroupList(TestConsistencyGroup):
 
