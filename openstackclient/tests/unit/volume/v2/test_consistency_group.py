1907220113efe9425a6cc7f52ce87dd4e82233c7
daizhiyong <zhiyong.dai@easystack.cn>
2016-11-16 15:39:14 +0800

    Add "consistency group show" command
    Add "consistency group show" command in volume v2 (v2 only).
    
    Change-Id: If496eba2955c0aacd52600bb6fba39690ddd90cb
    Implements: bp cinder-command-support
    Partial-Bug: #1613964

--- openstackclient/tests/unit/volume/v2/test_consistency_group.py
+++ openstackclient/tests/unit/volume/v2/test_consistency_group.py
@@ -353,3 +353,46 @@ class TestConsistencyGroupList(TestConsistencyGroup):
             detailed=True, search_opts={'all_tenants': False})
         self.assertEqual(self.columns_long, columns)
         self.assertEqual(self.data_long, list(data))
+
+
+class TestConsistencyGroupShow(TestConsistencyGroup):
+    columns = (
+        'availability_zone',
+        'created_at',
+        'description',
+        'id',
+        'name',
+        'status',
+        'volume_types',
+    )
+
+    def setUp(self):
+        super(TestConsistencyGroupShow, self).setUp()
+
+        self.consistency_group = (
+            volume_fakes.FakeConsistencyGroup.create_one_consistency_group())
+        self.data = (
+            self.consistency_group.availability_zone,
+            self.consistency_group.created_at,
+            self.consistency_group.description,
+            self.consistency_group.id,
+            self.consistency_group.name,
+            self.consistency_group.status,
+            self.consistency_group.volume_types,
+        )
+        self.consistencygroups_mock.get.return_value = self.consistency_group
+        self.cmd = consistency_group.ShowConsistencyGroup(self.app, None)
+
+    def test_consistency_group_show(self):
+        arglist = [
+            self.consistency_group.id
+        ]
+        verifylist = [
+            ("consistency_group", self.consistency_group.id)
+        ]
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+        columns, data = self.cmd.take_action(parsed_args)
+        self.consistencygroups_mock.get.assert_called_once_with(
+            self.consistency_group.id)
+        self.assertEqual(self.columns, columns)
+        self.assertEqual(self.data, data)
