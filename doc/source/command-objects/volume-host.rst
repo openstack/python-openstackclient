71e6d444767a29664821ab190e0736088d8ccfba
jiahui.qiang <jiahui.qiang@easystack.cn>
2016-11-21 05:47:48 +0800

    Add "volume host set" command
    Add "volume host set" command in volume v2 to support freeze(disable)
    and thaw(enable) the specified cinder-volume host
    
    Change-Id: Iee1604d72f9eccd9e327b0ef8d345909a733a647
    Implements: bp cinder-command-support

---
+++ doc/source/command-objects/volume-host.rst
@@ -0,0 +1,30 @@
+===========
+volume host
+===========
+
+Volume v2
+
+volume host set
+---------------
+
+Set volume host properties
+
+.. program:: volume host set
+.. code:: bash
+
+    os volume host set
+        [--enable | --disable]
+        <host-name>
+
+.. option:: --enable
+
+    Thaw and enable the specified volume host
+
+.. option:: --disable
+
+    Freeze and disable the specified volume host
+
+.. _volume-host-set:
+.. describe:: <host-name>
+
+    Name of volume host
---
+++ openstackclient/tests/unit/volume/v2/test_volume_host.py
@@ -0,0 +1,86 @@
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+#
+
+from openstackclient.tests.unit.volume.v2 import fakes as host_fakes
+from openstackclient.volume.v2 import volume_host
+
+
+class TestVolumeHost(host_fakes.TestVolume):
+
+    def setUp(self):
+        super(TestVolumeHost, self).setUp()
+
+        self.host_mock = self.app.client_manager.volume.services
+        self.host_mock.reset_mock()
+
+
+class TestVolumeHostSet(TestVolumeHost):
+
+    service = host_fakes.FakeService.create_one_service()
+
+    def setUp(self):
+        super(TestVolumeHostSet, self).setUp()
+
+        self.host_mock.freeze_host.return_value = None
+        self.host_mock.thaw_host.return_value = None
+
+        self.cmd = volume_host.SetVolumeHost(self.app, None)
+
+    def test_volume_host_set_nothing(self):
+        arglist = [
+            self.service.host,
+        ]
+        verifylist = [
+            ('host', self.service.host),
+        ]
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+        result = self.cmd.take_action(parsed_args)
+
+        self.host_mock.freeze_host.assert_not_called()
+        self.host_mock.thaw_host.assert_not_called()
+        self.assertIsNone(result)
+
+    def test_volume_host_set_enable(self):
+        arglist = [
+            '--enable',
+            self.service.host,
+        ]
+        verifylist = [
+            ('enable', True),
+            ('host', self.service.host),
+        ]
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+
+        result = self.cmd.take_action(parsed_args)
+
+        self.host_mock.thaw_host.assert_called_with(self.service.host)
+        self.host_mock.freeze_host.assert_not_called()
+        self.assertIsNone(result)
+
+    def test_volume_host_set_disable(self):
+        arglist = [
+            '--disable',
+            self.service.host,
+        ]
+        verifylist = [
+            ('disable', True),
+            ('host', self.service.host),
+        ]
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+
+        result = self.cmd.take_action(parsed_args)
+
+        self.host_mock.freeze_host.assert_called_with(self.service.host)
+        self.host_mock.thaw_host.assert_not_called()
+        self.assertIsNone(result)
