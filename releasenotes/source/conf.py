faf6e16120206de763c1570698f71114307dab98
Akihiro Motoki <amotoki@gmail.com>
2017-07-06 17:51:45 +0000

    Use openstackdocstheme in release note
    In the release notes build, openstackdocstheme is specified
    in the sphinx extension list, but it is actually not used.
    This commit configures openstackdocstheme as suggested in
    the openstackdocstheme document.
    
    Also specifies display_toc False in html_theme_options.
    This prevents openstackdocstheme to add the local TOC automatically.
    OSC has several releases during a single development cycle and
    it leads to a long TOC at the top of individual pages.
    It loses the readability and we can see version numbers in the
    left sidebar, so we can safely disable the local TOC in
    the release notes.
    
    [1] https://docs.openstack.org/openstackdocstheme/latest/
    
    Change-Id: Iae08d309f7589bb13a1766f6fded70673ba24047

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -43,6 +43,11 @@ extensions = [
     'sphinx.ext.extlinks',
 ]
 
+# openstackdocstheme options
+repository_name = 'openstack/python-openstackclient'
+bug_project = 'python-openstackclient'
+bug_tag = ''
+
 # Set aliases for extlinks
 # * lpbug - generic Launchpad bug        :lpbug:`123456`
 # * oscbp - OSC blueprints               :oscbp:`Blue Print <bp-name>`
@@ -132,12 +137,14 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'openstackdocs'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-# html_theme_options = {}
+html_theme_options = {
+    'display_toc': False,
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
