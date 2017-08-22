ff85c627078ec8d4770ba875ad35f5e63facb173
Akihiro Motoki <amotoki@gmail.com>
2017-08-22 21:38:07 +0000

    flake8-import-order: Ensure to place project imports last
    To ensure project imports are placed after third party import,
    we need to specify application-import-names.
    Previously flake8-import-check checked only standard imports or not.
    
    Change-Id: Iad7afa456cec7cf5b44955f1ea03c593a4c0e426

--- openstackclient/tests/unit/network/test_common.py
+++ openstackclient/tests/unit/network/test_common.py
@@ -14,8 +14,8 @@
 import argparse
 
 import mock
-
 import openstack
+
 from openstackclient.common import exceptions
 from openstackclient.network import common
 from openstackclient.tests.unit import utils
