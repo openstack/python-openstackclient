333e8abef33eec95330721cd5ae54243680d1612
Huang Cheng <huang.cheng@h3c.com>
2018-02-03 10:48:04 +0800

    Fix subnet host_routes error
    When updating subnet with "no-host-route" option, set host_routes to an empty list as neutron_lib.api.validators expected.
    
    Change-Id: I6fe039793d813758429c7a104fd40172b4f8122b
    Closes-Bug: #1747101
    (cherry picked from commit 5bb5585aa98e17e22963996c02bd7bd688d7871e)

--- openstackclient/tests/unit/network/v2/test_subnet.py
+++ openstackclient/tests/unit/network/v2/test_subnet.py
@@ -1046,6 +1046,36 @@ class TestSetSubnet(TestSubnet):
             _testsubnet, **attrs)
         self.assertIsNone(result)
 
+    def test_clear_options(self):
+        _testsubnet = network_fakes.FakeSubnet.create_one_subnet(
+            {'host_routes': [{'destination': '10.20.20.0/24',
+                              'nexthop': '10.20.20.1'}],
+             'allocation_pools': [{'start': '8.8.8.200',
+                                   'end': '8.8.8.250'}],
+             'dns_nameservers': ['10.0.0.1'], })
+        self.network.find_subnet = mock.Mock(return_value=_testsubnet)
+        arglist = [
+            '--no-host-route',
+            '--no-allocation-pool',
+            '--no-dns-nameservers',
+            _testsubnet.name,
+        ]
+        verifylist = [
+            ('no_dns_nameservers', True),
+            ('no_host_route', True),
+            ('no_allocation_pool', True),
+        ]
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+        result = self.cmd.take_action(parsed_args)
+        attrs = {
+            'host_routes': [],
+            'allocation_pools': [],
+            'dns_nameservers': [],
+        }
+        self.network.update_subnet.assert_called_once_with(
+            _testsubnet, **attrs)
+        self.assertIsNone(result)
+
     def _test_set_tags(self, with_tags=True):
         if with_tags:
             arglist = ['--tag', 'red', '--tag', 'blue']
