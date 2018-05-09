69b3a1545c54bd5e6972599e1a0656cd9f6d0cda
Julie Pichon <jpichon@redhat.com>
2018-05-09 17:43:09 +0100

    Prevent "server migrate --wait" from hanging
    Migrate uses the same mechanism in the backend than Resize and so the
    steps and step names are similar. Currently when using the --wait
    option with 'migrate', we wait forever because the status won't get to
    active until the user performs an action. This makes it return on
    verify_resize status just like 'resize' does, so that the user can
    perform the next manual step.
    
    Change-Id: Ie1aeac52506bc8801f88fd6a6eb4f6094cf20050
    Story: 2001994
    Task: 19620
    (cherry picked from commit 814ffb2a6f22aed640ba6adad62c1288f917b09a)

--- openstackclient/compute/v2/server.py
+++ openstackclient/compute/v2/server.py
@@ -1279,6 +1279,7 @@ class MigrateServer(command.Command):
             if utils.wait_for_status(
                 compute_client.servers.get,
                 server.id,
+                success_status=['active', 'verify_resize'],
                 callback=_show_progress,
             ):
                 sys.stdout.write(_('Complete\n'))
