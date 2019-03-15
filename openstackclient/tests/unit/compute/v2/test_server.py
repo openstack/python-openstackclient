01eb4e839394fe433a92a06daf1499fb00f2fe69
Sean Mooney <work@seanmooney.info>
2019-03-15 13:03:10 +0000

    Add 'openstack server evacuate' command
    This change adds a new 'openstack server evacuate' command to provide
    parity with the 'nova evacuate' command. The term "evacuate" is
    notoriously poor, in that it implies the instance is moved rather than
    recreated, but it is retained since people are familiar with it now.
    
    Change-Id: I1e32ca51036c501862d8e89b3144a9695d98a06f

--- openstackclient/tests/unit/compute/v2/test_server.py
+++ openstackclient/tests/unit/compute/v2/test_server.py
@@ -4982,6 +4982,167 @@ class TestServerRebuild(TestServer):
                           self.cmd, arglist, verifylist)
 
 
+class TestEvacuateServer(TestServer):
+
+    def setUp(self):
+        super(TestEvacuateServer, self).setUp()
+        # Return value for utils.find_resource for image
+        self.image = image_fakes.FakeImage.create_one_image()
+        self.images_mock.get.return_value = self.image
+
+        # Fake the rebuilt new server.
+        attrs = {
+            'image': {
+                'id': self.image.id
+            },
+            'networks': {},
+            'adminPass': 'passw0rd',
+        }
+        new_server = compute_fakes.FakeServer.create_one_server(attrs=attrs)
+
+        # Fake the server to be rebuilt. The IDs of them should be the same.
+        attrs['id'] = new_server.id
+        methods = {
+            'evacuate': new_server,
+        }
+        self.server = compute_fakes.FakeServer.create_one_server(
+            attrs=attrs,
+            methods=methods
+        )
+
+        # Return value for utils.find_resource for server.
+        self.servers_mock.get.return_value = self.server
+
+        self.cmd = server.EvacuateServer(self.app, None)
+
+    def _test_evacuate(self, args, verify_args, evac_args):
+        parsed_args = self.check_parser(self.cmd, args, verify_args)
+
+        # Get the command object to test
+        self.cmd.take_action(parsed_args)
+
+        self.servers_mock.get.assert_called_with(self.server.id)
+        self.server.evacuate.assert_called_with(**evac_args)
+
+    def test_evacuate(self):
+        args = [
+            self.server.id,
+        ]
+        verify_args = [
+            ('server', self.server.id),
+        ]
+        evac_args = {
+            'host': None, 'on_shared_storage': False, 'password': None,
+        }
+        self._test_evacuate(args, verify_args, evac_args)
+
+    def test_evacuate_with_password(self):
+        args = [
+            self.server.id,
+            '--password', 'password',
+        ]
+        verify_args = [
+            ('server', self.server.id),
+            ('password', 'password'),
+        ]
+        evac_args = {
+            'host': None, 'on_shared_storage': False, 'password': 'password',
+        }
+        self._test_evacuate(args, verify_args, evac_args)
+
+    def test_evacuate_with_host(self):
+        self.app.client_manager.compute.api_version = \
+            api_versions.APIVersion('2.29')
+
+        host = 'target-host'
+        args = [
+            self.server.id,
+            '--host', 'target-host',
+        ]
+        verify_args = [
+            ('server', self.server.id),
+            ('host', 'target-host'),
+        ]
+        evac_args = {'host': host, 'password': None}
+
+        self._test_evacuate(args, verify_args, evac_args)
+
+    def test_evacuate_with_host_pre_v229(self):
+        self.app.client_manager.compute.api_version = \
+            api_versions.APIVersion('2.28')
+
+        args = [
+            self.server.id,
+            '--host', 'target-host',
+        ]
+        verify_args = [
+            ('server', self.server.id),
+            ('host', 'target-host'),
+        ]
+        parsed_args = self.check_parser(self.cmd, args, verify_args)
+
+        self.assertRaises(
+            exceptions.CommandError,
+            self.cmd.take_action,
+            parsed_args)
+
+    def test_evacuate_without_share_storage(self):
+        self.app.client_manager.compute.api_version = \
+            api_versions.APIVersion('2.13')
+
+        args = [
+            self.server.id,
+            '--shared-storage'
+        ]
+        verify_args = [
+            ('server', self.server.id),
+            ('shared_storage', True),
+        ]
+        evac_args = {
+            'host': None, 'on_shared_storage': True, 'password': None,
+        }
+        self._test_evacuate(args, verify_args, evac_args)
+
+    def test_evacuate_without_share_storage_post_v213(self):
+        self.app.client_manager.compute.api_version = \
+            api_versions.APIVersion('2.14')
+
+        args = [
+            self.server.id,
+            '--shared-storage'
+        ]
+        verify_args = [
+            ('server', self.server.id),
+            ('shared_storage', True),
+        ]
+        parsed_args = self.check_parser(self.cmd, args, verify_args)
+
+        self.assertRaises(
+            exceptions.CommandError,
+            self.cmd.take_action,
+            parsed_args)
+
+    @mock.patch.object(common_utils, 'wait_for_status', return_value=True)
+    def test_evacuate_with_wait_ok(self, mock_wait_for_status):
+        args = [
+            self.server.id,
+            '--wait',
+        ]
+        verify_args = [
+            ('server', self.server.id),
+            ('wait', True),
+        ]
+        evac_args = {
+            'host': None, 'on_shared_storage': False, 'password': None,
+        }
+        self._test_evacuate(args, verify_args, evac_args)
+        mock_wait_for_status.assert_called_once_with(
+            self.servers_mock.get,
+            self.server.id,
+            callback=mock.ANY,
+        )
+
+
 class TestServerRemoveFixedIP(TestServer):
 
     def setUp(self):
