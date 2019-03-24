f750ff452d82edf65d693214d542f6c1106e4e9c
Ian Wienand <iwienand@redhat.com>
2019-03-24 20:35:55 +0000

    Replace openstack.org git:// URLs with https://
    This is a mechanically generated change to replace openstack.org
    git:// URLs with https:// equivalents.
    
    This is in aid of a planned future move of the git hosting
    infrastructure to a self-hosted instance of gitea (https://gitea.io),
    which does not support the git wire protocol at this stage.
    
    This update should result in no functional change.
    
    For more information see the thread at
    
     http://lists.openstack.org/pipermail/openstack-discuss/2019-March/003825.html
    
    Change-Id: I96de94e04c78b2a39d319a4951731c7095cdc5f3

--- tools/tox_install.sh
+++ tools/tox_install.sh
@@ -33,7 +33,7 @@ elif [ -x "$ZUUL_CLONER" ]; then
     $ZUUL_CLONER --cache-dir \
         /opt/git \
         --branch $BRANCH_NAME \
-        git://git.openstack.org \
+        https://git.openstack.org \
         openstack/requirements
     cd openstack/requirements
     $install_cmd -e .
