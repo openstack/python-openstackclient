1701768c2d5319c064cbdb22d1a84ae37116746c
Stephen Finucane <sfinucan@redhat.com>
2022-12-19 16:43:47 +0000

    Remove unused helpers for legacy novaclient
    These are no longer used.
    
    Change-Id: I9514e98a5549130c12f3b785e90debf3437e6933
    Signed-off-by: Stephen Finucane <sfinucan@redhat.com>

--- openstackclient/tests/unit/compute/v2/test_server.py
+++ openstackclient/tests/unit/compute/v2/test_server.py
@@ -175,26 +175,6 @@ class TestServer(compute_fakes.TestComputev2):
 
         return volumes
 
-    def run_method_with_servers(self, method_name, server_count):
-        servers = self.setup_servers_mock(server_count)
-
-        arglist = []
-        verifylist = []
-
-        for s in servers:
-            arglist.append(s.id)
-        verifylist = [
-            ('server', arglist),
-        ]
-        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
-
-        result = self.cmd.take_action(parsed_args)
-
-        for s in servers:
-            method = getattr(s, method_name)
-            method.assert_called_with()
-        self.assertIsNone(result)
-
     def run_method_with_sdk_servers(self, method_name, server_count):
         servers = self.setup_sdk_servers_mock(count=server_count)
 
