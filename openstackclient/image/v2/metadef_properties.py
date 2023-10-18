705ecef7a949ef6df0401fe98b66d1c8d96de1d6
Cyril Roelandt <cyril@redhat.com>
2023-10-18 11:55:49 +0000

    Add "image metadef property set" command
    Change-Id: I8fbe8ef5b5119fb500df0ed1b6e645ea00eadf01

--- openstackclient/image/v2/metadef_properties.py
+++ openstackclient/image/v2/metadef_properties.py
@@ -173,6 +173,68 @@ class ListMetadefProperties(command.Lister):
         )
 
 
+class SetMetadefProperty(command.Command):
+    _description = _("Update metadef namespace property")
+
+    def get_parser(self, prog_name):
+        parser = super().get_parser(prog_name)
+        parser.add_argument(
+            "--name",
+            help=_("Internal name of the property"),
+        )
+        parser.add_argument(
+            "--title",
+            help=_("Property name displayed to the user"),
+        )
+        parser.add_argument(
+            "--type",
+            help=_("Property type"),
+        )
+        parser.add_argument(
+            "--schema",
+            help=_("Valid JSON schema of the property"),
+        )
+        parser.add_argument(
+            "namespace_name",
+            help=_("Namespace of the namespace to which the property belongs"),
+        )
+        parser.add_argument(
+            "property_name",
+            help=_("Property to update"),
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        image_client = self.app.client_manager.image
+
+        # We need to pass the values for *all* attributes as kwargs to
+        # update_metadef_property(), otherwise the attributes that are not
+        # listed will be reset.
+        data = image_client.get_metadef_property(
+            parsed_args.property_name, parsed_args.namespace_name
+        )
+        kwargs = _format_property(data)
+        for key in ['name', 'title', 'type']:
+            argument = getattr(parsed_args, key, None)
+            if argument is not None:
+                kwargs[key] = argument
+
+        if parsed_args.schema:
+            try:
+                kwargs.update(json.loads(parsed_args.schema))
+            except json.JSONDecodeError as e:
+                raise exceptions.CommandError(
+                    _("Failed to load JSON schema: %(e)s")
+                    % {
+                        'e': e,
+                    }
+                )
+
+        image_client.update_metadef_property(
+            parsed_args.property_name, parsed_args.namespace_name, **kwargs
+        )
+
+
 class ShowMetadefProperty(command.ShowOne):
     _description = _("Describe a specific property from a namespace")
 
