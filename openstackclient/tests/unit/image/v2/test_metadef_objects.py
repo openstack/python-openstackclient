3cd5ad2c1babc9bddc85c1faf5005728a909725c
Mridula Joshi <mrjoshi@redhat.com>
2023-12-20 07:35:33 +0000

    Adds CLI support for ``glance md-object-update``
    This patch adds a command "image metadef object update" which
    update metadata definitions object inside a namespace.
    
    Change-Id: I2dd8f54f9224abda5adc7a1b6a1c270c49d473a7

--- openstackclient/tests/unit/image/v2/test_metadef_objects.py
+++ openstackclient/tests/unit/image/v2/test_metadef_objects.py
@@ -160,3 +160,41 @@ class TestMetadefObjectList(fakes.TestImagev2):
 
         self.assertEqual(self.columns, columns)
         self.assertEqual(getattr(self.datalist[0], 'name'), next(data)[0])
+
+
+class TestMetadefObjectSet(fakes.TestImagev2):
+    _metadef_namespace = fakes.create_one_metadef_namespace()
+    _metadef_objects = fakes.create_one_metadef_object()
+    new_metadef_object = fakes.create_one_metadef_object(
+        attrs={'name': 'new_object_name'}
+    )
+
+    def setUp(self):
+        super().setUp()
+
+        self.image_client.update_metadef_object.return_value = None
+        self.cmd = metadef_objects.SetMetadefObject(self.app, None)
+
+    def test_object_set_no_options(self):
+        arglist = [
+            self._metadef_namespace.namespace,
+            self._metadef_objects.name,
+        ]
+        verifylist = []
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+        result = self.cmd.take_action(parsed_args)
+
+        self.assertIsNone(result)
+
+    def test_object_set(self):
+        arglist = [
+            self._metadef_namespace.namespace,
+            self._metadef_objects.name,
+            '--name',
+            'new_object_name',
+        ]
+        verifylist = []
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+        result = self.cmd.take_action(parsed_args)
+
+        self.assertIsNone(result)
