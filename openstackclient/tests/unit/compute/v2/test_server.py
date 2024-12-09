22b30b99ce0e7b64aed1a13df73ab52a0b33204d
Stephen Finucane <stephenfin@redhat.com>
2024-12-09 13:32:15 +0000

    compute: Workaround bug #2089821
    By passing a dict instead of a single value, we force SDK to populate
    the correct attribute on the object.
    
    Change-Id: I9f4c5964dc0546215474c92db567966ffad68a1a
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>
    Related-bug: #2089821

--- openstackclient/tests/unit/compute/v2/test_server.py
+++ openstackclient/tests/unit/compute/v2/test_server.py
@@ -1165,7 +1165,7 @@ class TestServerAddSecurityGroup(compute_fakes.TestComputev2):
             self.server.id, ignore_missing=False
         )
         self.compute_sdk_client.add_security_group_to_server.assert_called_once_with(
-            self.server, 'fake_sg'
+            self.server, {'name': 'fake_sg'}
         )
         mock_find_nova_net_sg.assert_called_once_with(
             self.compute_sdk_client, 'fake_sg'
@@ -1186,7 +1186,7 @@ class TestServerAddSecurityGroup(compute_fakes.TestComputev2):
             self.server.id, ignore_missing=False
         )
         self.compute_sdk_client.add_security_group_to_server.assert_called_once_with(
-            self.server, 'fake_sg'
+            self.server, {'name': 'fake_sg'}
         )
         self.assertIsNone(result)
 
@@ -7400,7 +7400,7 @@ class TestServerRemoveSecurityGroup(TestServer):
             self.server.id, ignore_missing=False
         )
         self.compute_sdk_client.remove_security_group_from_server.assert_called_once_with(
-            self.server, 'fake_sg'
+            self.server, {'name': 'fake_sg'}
         )
         mock_find_nova_net_sg.assert_called_once_with(
             self.compute_sdk_client, 'fake_sg'
@@ -7421,7 +7421,7 @@ class TestServerRemoveSecurityGroup(TestServer):
             self.server.id, ignore_missing=False
         )
         self.compute_sdk_client.remove_security_group_from_server.assert_called_once_with(
-            self.server, 'fake_sg'
+            self.server, {'name': 'fake_sg'}
         )
         self.assertIsNone(result)
 
