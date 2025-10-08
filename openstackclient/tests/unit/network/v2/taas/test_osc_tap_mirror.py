97c2238df17e7e586f00a5fb06da4ec008ad834d
Miro Tomaska <mtomaska@redhat.com>
2025-10-08 15:27:38 -0400

    Moving tapas osc client code from neutronclient
    Proposal to move all stadium projects from neutronclient
    to openstackclient repo. Tap-as-a-service is the first example.
    The tapas osc client code was recently moved to neutronclient see
    https://review.opendev.org/c/openstack/tap-as-a-service/+/960849
    but proposal is to make openstackclient its final destination.
    
    This change also includes automatic lint fixes required in
    this repo.
    
    Change-Id: Ied47f40c6947600d40bf675ec06f0bf88fd15f1f
    Signed-off-by: Miro Tomaska <mtomaska@redhat.com>

---
+++ openstackclient/tests/unit/network/v2/taas/test_osc_tap_mirror.py
@@ -0,0 +1,283 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import copy
+import operator
+import uuid
+
+from openstack.network.v2 import tap_mirror
+from osc_lib import utils as osc_utils
+from osc_lib.utils import columns as column_util
+
+from openstackclient.network.v2.taas import tap_mirror as osc_tap_mirror
+from openstackclient.tests.unit.network.v2 import fakes as network_fakes
+from openstackclient.tests.unit.network.v2.taas import fakes
+
+
+columns_long = tuple(
+    col
+    for col, _, listing_mode in osc_tap_mirror._attr_map
+    if listing_mode in (column_util.LIST_BOTH, column_util.LIST_LONG_ONLY)
+)
+headers_long = tuple(
+    head
+    for _, head, listing_mode in osc_tap_mirror._attr_map
+    if listing_mode in (column_util.LIST_BOTH, column_util.LIST_LONG_ONLY)
+)
+sorted_attr_map = sorted(osc_tap_mirror._attr_map, key=operator.itemgetter(1))
+sorted_columns = tuple(col for col, _, _ in sorted_attr_map)
+sorted_headers = tuple(head for _, head, _ in sorted_attr_map)
+
+
+def _get_data(attrs, columns=sorted_columns):
+    return osc_utils.get_dict_properties(attrs, columns)
+
+
+class TestCreateTapMirror(network_fakes.TestNetworkV2):
+    columns = (
+        'directions',
+        'id',
+        'mirror_type',
+        'name',
+        'port_id',
+        'remote_ip',
+    )
+
+    def setUp(self):
+        super().setUp()
+        self.cmd = osc_tap_mirror.CreateTapMirror(self.app, None)
+
+    def test_create_tap_mirror(self):
+        port_id = str(uuid.uuid4())
+        fake_tap_mirror = fakes.FakeTapMirror.create_tap_mirror(
+            attrs={'port_id': port_id}
+        )
+        self.app.client_manager.network.create_tap_mirror.return_value = (
+            fake_tap_mirror
+        )
+        self.app.client_manager.network.find_port.return_value = {
+            'id': port_id
+        }
+        self.app.client_manager.network.find_tap_mirror.side_effect = (
+            lambda _, name_or_id: {'id': name_or_id}
+        )
+        arg_list = [
+            '--name',
+            fake_tap_mirror['name'],
+            '--port',
+            fake_tap_mirror['port_id'],
+            '--directions',
+            fake_tap_mirror['directions'],
+            '--remote-ip',
+            fake_tap_mirror['remote_ip'],
+            '--mirror-type',
+            fake_tap_mirror['mirror_type'],
+        ]
+
+        verify_directions = fake_tap_mirror['directions'].split('=')
+        verify_directions_dict = {verify_directions[0]: verify_directions[1]}
+
+        verify_list = [
+            ('name', fake_tap_mirror['name']),
+            ('port_id', fake_tap_mirror['port_id']),
+            ('directions', verify_directions_dict),
+            ('remote_ip', fake_tap_mirror['remote_ip']),
+            ('mirror_type', fake_tap_mirror['mirror_type']),
+        ]
+
+        parsed_args = self.check_parser(self.cmd, arg_list, verify_list)
+        self.app.client_manager.network.find_tap_mirror.return_value = (
+            fake_tap_mirror
+        )
+
+        columns, data = self.cmd.take_action(parsed_args)
+        create_tap_m_mock = self.app.client_manager.network.create_tap_mirror
+        create_tap_m_mock.assert_called_once_with(
+            **{
+                'name': fake_tap_mirror['name'],
+                'port_id': fake_tap_mirror['port_id'],
+                'directions': verify_directions_dict,
+                'remote_ip': fake_tap_mirror['remote_ip'],
+                'mirror_type': fake_tap_mirror['mirror_type'],
+            }
+        )
+        self.assertEqual(self.columns, columns)
+        fake_data = _get_data(
+            fake_tap_mirror, osc_tap_mirror._get_columns(fake_tap_mirror)[1]
+        )
+        self.assertEqual(fake_data, data)
+
+
+class TestListTapMirror(network_fakes.TestNetworkV2):
+    def setUp(self):
+        super().setUp()
+        self.cmd = osc_tap_mirror.ListTapMirror(self.app, None)
+
+    def test_list_tap_mirror(self):
+        """Test List Tap Mirror."""
+        fake_tap_mirrors = fakes.FakeTapMirror.create_tap_mirrors(
+            attrs={'port_id': str(uuid.uuid4())}, count=4
+        )
+        self.app.client_manager.network.tap_mirrors.return_value = (
+            fake_tap_mirrors
+        )
+
+        arg_list = []
+        verify_list = []
+
+        parsed_args = self.check_parser(self.cmd, arg_list, verify_list)
+
+        headers, data = self.cmd.take_action(parsed_args)
+
+        self.app.client_manager.network.tap_mirrors.assert_called_once()
+        self.assertEqual(headers, list(headers_long))
+        self.assertCountEqual(
+            list(data),
+            [
+                _get_data(fake_tap_mirror, columns_long)
+                for fake_tap_mirror in fake_tap_mirrors
+            ],
+        )
+
+
+class TestDeleteTapMirror(network_fakes.TestNetworkV2):
+    def setUp(self):
+        super().setUp()
+        self.app.client_manager.network.find_tap_mirror.side_effect = (
+            lambda name_or_id, ignore_missing: tap_mirror.TapMirror(
+                id=name_or_id
+            )
+        )
+        self.cmd = osc_tap_mirror.DeleteTapMirror(self.app, None)
+
+    def test_delete_tap_mirror(self):
+        """Test Delete Tap Mirror."""
+
+        fake_tap_mirror = fakes.FakeTapMirror.create_tap_mirror(
+            attrs={'port_id': str(uuid.uuid4())}
+        )
+
+        arg_list = [
+            fake_tap_mirror['id'],
+        ]
+        verify_list = [
+            (osc_tap_mirror.TAP_MIRROR, [fake_tap_mirror['id']]),
+        ]
+
+        parsed_args = self.check_parser(self.cmd, arg_list, verify_list)
+        result = self.cmd.take_action(parsed_args)
+
+        mock_delete_tap_m = self.app.client_manager.network.delete_tap_mirror
+        mock_delete_tap_m.assert_called_once_with(fake_tap_mirror['id'])
+        self.assertIsNone(result)
+
+
+class TestShowTapMirror(network_fakes.TestNetworkV2):
+    columns = (
+        'directions',
+        'id',
+        'mirror_type',
+        'name',
+        'port_id',
+        'remote_ip',
+    )
+
+    def setUp(self):
+        super().setUp()
+        self.app.client_manager.network.find_tap_mirror.side_effect = (
+            lambda name_or_id, ignore_missing: tap_mirror.TapMirror(
+                id=name_or_id
+            )
+        )
+        self.cmd = osc_tap_mirror.ShowTapMirror(self.app, None)
+
+    def test_show_tap_mirror(self):
+        """Test Show Tap Mirror."""
+
+        fake_tap_mirror = fakes.FakeTapMirror.create_tap_mirror(
+            attrs={'port_id': str(uuid.uuid4())}
+        )
+        self.app.client_manager.network.get_tap_mirror.return_value = (
+            fake_tap_mirror
+        )
+        arg_list = [
+            fake_tap_mirror['id'],
+        ]
+        verify_list = [
+            (osc_tap_mirror.TAP_MIRROR, fake_tap_mirror['id']),
+        ]
+
+        parsed_args = self.check_parser(self.cmd, arg_list, verify_list)
+
+        headers, data = self.cmd.take_action(parsed_args)
+
+        mock_get_tap_m = self.app.client_manager.network.get_tap_mirror
+        mock_get_tap_m.assert_called_once_with(fake_tap_mirror['id'])
+        self.assertEqual(self.columns, headers)
+        fake_data = _get_data(
+            fake_tap_mirror, osc_tap_mirror._get_columns(fake_tap_mirror)[1]
+        )
+        self.assertEqual(fake_data, data)
+
+
+class TestUpdateTapMirror(network_fakes.TestNetworkV2):
+    _new_name = 'new_name'
+    columns = (
+        'directions',
+        'id',
+        'mirror_type',
+        'name',
+        'port_id',
+        'remote_ip',
+    )
+
+    def setUp(self):
+        super().setUp()
+        self.cmd = osc_tap_mirror.UpdateTapMirror(self.app, None)
+        self.app.client_manager.network.find_tap_mirror.side_effect = (
+            lambda name_or_id, ignore_missing: tap_mirror.TapMirror(
+                id=name_or_id
+            )
+        )
+
+    def test_update_tap_mirror(self):
+        """Test update Tap Mirror"""
+        fake_tap_mirror = fakes.FakeTapMirror.create_tap_mirror(
+            attrs={'port_id': str(uuid.uuid4())}
+        )
+        new_tap_mirror = copy.deepcopy(fake_tap_mirror)
+        new_tap_mirror['name'] = self._new_name
+
+        self.app.client_manager.network.update_tap_mirror.return_value = (
+            new_tap_mirror
+        )
+
+        arg_list = [
+            fake_tap_mirror['id'],
+            '--name',
+            self._new_name,
+        ]
+        verify_list = [('name', self._new_name)]
+
+        parsed_args = self.check_parser(self.cmd, arg_list, verify_list)
+        columns, data = self.cmd.take_action(parsed_args)
+        attrs = {'name': self._new_name}
+
+        mock_update_tap_m = self.app.client_manager.network.update_tap_mirror
+        mock_update_tap_m.assert_called_once_with(
+            fake_tap_mirror['id'], **attrs
+        )
+        self.assertEqual(self.columns, columns)
+        fake_data = _get_data(
+            new_tap_mirror, osc_tap_mirror._get_columns(new_tap_mirror)[1]
+        )
+        self.assertEqual(fake_data, data)
---
+++ openstackclient/tests/unit/network/v2/taas/test_osc_tap_service.py
@@ -0,0 +1,266 @@
+# All Rights Reserved 2020
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import copy
+import operator
+import uuid
+
+from openstack.network.v2 import tap_service
+from osc_lib import utils as osc_utils
+from osc_lib.utils import columns as column_util
+
+from openstackclient.network.v2.taas import tap_service as osc_tap_service
+from openstackclient.tests.unit.network.v2 import fakes as network_fakes
+from openstackclient.tests.unit.network.v2.taas import fakes
+
+
+columns_long = tuple(
+    col
+    for col, _, listing_mode in osc_tap_service._attr_map
+    if listing_mode in (column_util.LIST_BOTH, column_util.LIST_LONG_ONLY)
+)
+headers_long = tuple(
+    head
+    for _, head, listing_mode in osc_tap_service._attr_map
+    if listing_mode in (column_util.LIST_BOTH, column_util.LIST_LONG_ONLY)
+)
+sorted_attr_map = sorted(osc_tap_service._attr_map, key=operator.itemgetter(1))
+sorted_columns = tuple(col for col, _, _ in sorted_attr_map)
+sorted_headers = tuple(head for _, head, _ in sorted_attr_map)
+
+
+def _get_data(attrs, columns=sorted_columns):
+    return osc_utils.get_dict_properties(attrs, columns)
+
+
+class TestCreateTapService(network_fakes.TestNetworkV2):
+    columns = (
+        'id',
+        'name',
+        'port_id',
+        'status',
+    )
+
+    def setUp(self):
+        super().setUp()
+        self.cmd = osc_tap_service.CreateTapService(self.app, None)
+
+    def test_create_tap_service(self):
+        """Test Create Tap Service."""
+        port_id = str(uuid.uuid4())
+        fake_tap_service = fakes.FakeTapService.create_tap_service(
+            attrs={'port_id': port_id}
+        )
+        self.app.client_manager.network.create_tap_service.return_value = (
+            fake_tap_service
+        )
+        self.app.client_manager.network.find_port.return_value = {
+            'id': port_id
+        }
+        self.app.client_manager.network.find_tap_service.side_effect = (
+            lambda _, name_or_id: {'id': name_or_id}
+        )
+        arg_list = [
+            '--name',
+            fake_tap_service['name'],
+            '--port',
+            fake_tap_service['port_id'],
+        ]
+
+        verify_list = [
+            ('name', fake_tap_service['name']),
+            ('port_id', fake_tap_service['port_id']),
+        ]
+
+        parsed_args = self.check_parser(self.cmd, arg_list, verify_list)
+        self.app.client_manager.network.find_tap_service.return_value = (
+            fake_tap_service
+        )
+
+        columns, data = self.cmd.take_action(parsed_args)
+        create_tap_s_mock = self.app.client_manager.network.create_tap_service
+        create_tap_s_mock.assert_called_once_with(
+            **{
+                'name': fake_tap_service['name'],
+                'port_id': fake_tap_service['port_id'],
+            }
+        )
+        self.assertEqual(self.columns, columns)
+        fake_data = _get_data(
+            fake_tap_service, osc_tap_service._get_columns(fake_tap_service)[1]
+        )
+        self.assertEqual(fake_data, data)
+
+
+class TestListTapService(network_fakes.TestNetworkV2):
+    def setUp(self):
+        super().setUp()
+        self.cmd = osc_tap_service.ListTapService(self.app, None)
+
+    def test_list_tap_service(self):
+        """Test List Tap Service."""
+        fake_tap_services = fakes.FakeTapService.create_tap_services(
+            attrs={'port_id': str(uuid.uuid4())}, count=4
+        )
+        self.app.client_manager.network.tap_services.return_value = (
+            fake_tap_services
+        )
+
+        arg_list = []
+        verify_list = []
+
+        parsed_args = self.check_parser(self.cmd, arg_list, verify_list)
+
+        headers, data = self.cmd.take_action(parsed_args)
+
+        self.app.client_manager.network.tap_services.assert_called_once()
+        self.assertEqual(headers, list(headers_long))
+        self.assertCountEqual(
+            list(data),
+            [
+                _get_data(fake_tap_service, columns_long)
+                for fake_tap_service in fake_tap_services
+            ],
+        )
+
+
+class TestDeleteTapService(network_fakes.TestNetworkV2):
+    def setUp(self):
+        super().setUp()
+        self.app.client_manager.network.find_tap_service.side_effect = (
+            lambda name_or_id, ignore_missing: tap_service.TapService(
+                id=name_or_id
+            )
+        )
+        self.cmd = osc_tap_service.DeleteTapService(self.app, None)
+
+    def test_delete_tap_service(self):
+        """Test Delete tap service."""
+
+        fake_tap_service = fakes.FakeTapService.create_tap_service(
+            attrs={'port_id': str(uuid.uuid4())}
+        )
+
+        arg_list = [
+            fake_tap_service['id'],
+        ]
+        verify_list = [
+            (osc_tap_service.TAP_SERVICE, [fake_tap_service['id']]),
+        ]
+
+        parsed_args = self.check_parser(self.cmd, arg_list, verify_list)
+        result = self.cmd.take_action(parsed_args)
+
+        mock_delete_tap_s = self.app.client_manager.network.delete_tap_service
+        mock_delete_tap_s.assert_called_once_with(fake_tap_service['id'])
+        self.assertIsNone(result)
+
+
+class TestShowTapService(network_fakes.TestNetworkV2):
+    columns = (
+        'id',
+        'name',
+        'port_id',
+        'status',
+    )
+
+    def setUp(self):
+        super().setUp()
+        self.app.client_manager.network.find_tap_service.side_effect = (
+            lambda name_or_id, ignore_missing: tap_service.TapService(
+                id=name_or_id
+            )
+        )
+        self.cmd = osc_tap_service.ShowTapService(self.app, None)
+
+    def test_show_tap_service(self):
+        """Test Show tap service."""
+
+        fake_tap_service = fakes.FakeTapService.create_tap_service(
+            attrs={'port_id': str(uuid.uuid4())}
+        )
+        self.app.client_manager.network.get_tap_service.return_value = (
+            fake_tap_service
+        )
+        arg_list = [
+            fake_tap_service['id'],
+        ]
+        verify_list = [
+            (osc_tap_service.TAP_SERVICE, fake_tap_service['id']),
+        ]
+
+        parsed_args = self.check_parser(self.cmd, arg_list, verify_list)
+
+        headers, data = self.cmd.take_action(parsed_args)
+
+        mock_get_tap_s = self.app.client_manager.network.get_tap_service
+        mock_get_tap_s.assert_called_once_with(fake_tap_service['id'])
+        self.assertEqual(self.columns, headers)
+        fake_data = _get_data(
+            fake_tap_service, osc_tap_service._get_columns(fake_tap_service)[1]
+        )
+        self.assertEqual(fake_data, data)
+
+
+class TestUpdateTapService(network_fakes.TestNetworkV2):
+    _new_name = 'new_name'
+
+    columns = (
+        'id',
+        'name',
+        'port_id',
+        'status',
+    )
+
+    def setUp(self):
+        super().setUp()
+        self.cmd = osc_tap_service.UpdateTapService(self.app, None)
+        self.app.client_manager.network.find_tap_service.side_effect = (
+            lambda name_or_id, ignore_missing: tap_service.TapService(
+                id=name_or_id
+            )
+        )
+
+    def test_update_tap_service(self):
+        """Test update tap service"""
+        fake_tap_service = fakes.FakeTapService.create_tap_service(
+            attrs={'port_id': str(uuid.uuid4())}
+        )
+        new_tap_service = copy.deepcopy(fake_tap_service)
+        new_tap_service['name'] = self._new_name
+
+        self.app.client_manager.network.update_tap_service.return_value = (
+            new_tap_service
+        )
+
+        arg_list = [
+            fake_tap_service['id'],
+            '--name',
+            self._new_name,
+        ]
+        verify_list = [('name', self._new_name)]
+
+        parsed_args = self.check_parser(self.cmd, arg_list, verify_list)
+        columns, data = self.cmd.take_action(parsed_args)
+        attrs = {'name': self._new_name}
+
+        mock_update_tap_s = self.app.client_manager.network.update_tap_service
+        mock_update_tap_s.assert_called_once_with(
+            fake_tap_service['id'], **attrs
+        )
+        self.assertEqual(self.columns, columns)
+        fake_data = _get_data(
+            new_tap_service, osc_tap_service._get_columns(new_tap_service)[1]
+        )
+        self.assertEqual(fake_data, data)
