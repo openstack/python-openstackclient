05ffb117516e4758fb994a4a9211d46bac3be226
hong-p <inyong.hong@samsung.com>
2026-05-09 19:14:22 +0900

    fix: use correct parsed_args attribute in volume group type show
    The ShowVolumeGroupType.take_action() was referencing parsed_args.group
    instead of parsed_args.group_type, causing an AttributeError on every
    invocation.
    
    Closes-Bug: #2150856
    Change-Id: I6924040580af2bc2f30e2bc5f03a366cbb9fe6ef
    Signed-off-by: hongp <inyong.hong@samsung.com>

--- openstackclient/tests/unit/volume/v3/test_volume_group_type.py
+++ openstackclient/tests/unit/volume/v3/test_volume_group_type.py
@@ -459,3 +459,68 @@ class TestVolumeGroupTypeList(TestVolumeGroupType):
         self.assertIn(
             '--os-volume-api-version 3.11 or greater is required', str(exc)
         )
+
+
+class TestVolumeGroupTypeShow(TestVolumeGroupType):
+    fake_volume_group_type = volume_fakes.create_one_volume_group_type()
+
+    columns = (
+        'ID',
+        'Name',
+        'Description',
+        'Is Public',
+        'Properties',
+    )
+    data = (
+        fake_volume_group_type.id,
+        fake_volume_group_type.name,
+        fake_volume_group_type.description,
+        fake_volume_group_type.is_public,
+        format_columns.DictColumn(fake_volume_group_type.group_specs),
+    )
+
+    def setUp(self):
+        super().setUp()
+
+        self.volume_group_types_mock.get.return_value = (
+            self.fake_volume_group_type
+        )
+
+        self.cmd = volume_group_type.ShowVolumeGroupType(self.app, None)
+
+    def test_volume_group_type_show(self):
+        self.set_volume_api_version('3.11')
+
+        arglist = [
+            self.fake_volume_group_type.id,
+        ]
+        verifylist = [
+            ('group_type', self.fake_volume_group_type.id),
+        ]
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+
+        columns, data = self.cmd.take_action(parsed_args)
+
+        self.volume_group_types_mock.get.assert_called_once_with(
+            self.fake_volume_group_type.id,
+        )
+        self.assertEqual(self.columns, columns)
+        self.assertCountEqual(self.data, data)
+
+    def test_volume_group_type_show_pre_v311(self):
+        self.set_volume_api_version('3.10')
+
+        arglist = [
+            self.fake_volume_group_type.id,
+        ]
+        verifylist = [
+            ('group_type', self.fake_volume_group_type.id),
+        ]
+        parsed_args = self.check_parser(self.cmd, arglist, verifylist)
+
+        exc = self.assertRaises(
+            exceptions.CommandError, self.cmd.take_action, parsed_args
+        )
+        self.assertIn(
+            '--os-volume-api-version 3.11 or greater is required', str(exc)
+        )
